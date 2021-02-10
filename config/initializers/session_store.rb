1cf455798646749748ffea59bb78982fa37acb5a
asn-athira <athira@rightsolution.ae>
2021-02-10 15:07:48 +0530

    modified

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: 'https://rails-client-api.herokuapp.com', domain: 'https://react-auth-1qmu2kgvc.vercel.app'
+  Rails.application.config.session_store :cookie_store, key: '', domain: 'https://react-auth-1qmu2kgvc.vercel.app'
 else
-  Rails.application.config.session_store :cookie_store, key: 'https://rails-client-api.herokuapp.com/' 
+  Rails.application.config.session_store :cookie_store, key: '' 
 end
