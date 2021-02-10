44967fa234b2c910c4e1a2e75bf8e1c207b0b076
asn-athira <athira@rightsolution.ae>
2021-02-10 16:27:43 +0530

    modified

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '', domain: 'https://react-auth.asn-athira.vercel.app'
+  Rails.application.config.session_store :cookie_store, key: '', domain: 'https://react-auth-eta.vercel.app'
 else
   Rails.application.config.session_store :cookie_store, key: '' 
 end
