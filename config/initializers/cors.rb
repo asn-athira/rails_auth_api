35de1cc123606e98414c44ff44954acca9b19f83
asn-athira <athira@rightsolution.ae>
2021-02-10 16:03:35 +0530

    modified

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'https://react-auth-1qmu2kgvc.vercel.app'
+    origins 'https://react-auth.asn-athira.vercel.app/'
   
     resource '*',
       headers: :any,
